6bacfe7558b669842fad24469e479c30daa6b33b
EunSeong <seoun2819@naver.com>
2021-02-24 23:25:07 +0900

    test_21.20.24

---
+++ 1017.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{ 
+	int h,m;
+	scanf("%d %d", &h,&m);
+	printf("%d:%m",&h,&m);
+	
+	return 0;
+}
---
+++ 1020.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{ 
+	int f,r;
+	scanf("%d-%d", &f,&r); 
+	printf("%06d%d",f,r);
+	
+	return 0;
+}
