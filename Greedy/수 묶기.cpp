39ec45d224c0cd2a95c9ac207a9bbdf41d5e99f4
EunSeong <seoun4612@gmail.com>
2021-04-06 00:00:06 +0900

    practic_21.04.05

--- Greedy/수 묶기.cpp
+++ Greedy/수 묶기.cpp
@@ -1,51 +1,133 @@
 #include <iostream>
+
 #include <vector>
+
+#include <queue>
+
+#include <functional>
+
 #include <algorithm>
 
 using namespace std;
 
- int n;
- int res=0;
  
- bool cmp(int x, int y){
- 	return x > y;
- }
- 
- int main(){
- 	cin>>n;
- 	int a[n];
- 	bool d[n];	
- 	
- 	for (int i=0; i<n;i++){
- 		int x;
- 		cin>>x;
- 		a[i]=x;
- 		d[i]=false;
-	 }
-	 
-	sort(a,a+n,cmp);
 
-	 for (int i=0;i<n;i++){
-	 	if(d[i]==true)continue;
-	 	if(i!=n-1){
-			if(a[i]>0&&a[i+1]>0){
-				res += (a[i]*a[i+1]);
-				d[i]= true;
-				d[i+1]= true;
-			}
-			else {
-				res+=a[i]+a[i+1];
-				d[i]= true;
-				d[i+1]=true;
-			}
-			
-		 }
-		else{
-			res+=a[i];
-			 d[i]=true;
-	 }
-	 }
- 	
- 	cout<<res<<endl;
- 	
- }
+int N;
+
+priority_queue<int, vector<int>, greater<int>> positive;
+
+priority_queue<int> negative;
+
+ 
+
+int main(void)
+
+{
+
+        ios_base::sync_with_stdio(0);
+
+        cin.tie(0); //cin 속도 향상
+
+        cin >> N;
+
+ 
+
+        int one = 0, zero = 0;
+
+        for (int i = 0; i < N; i++)
+
+        {
+
+                 int num;
+
+                 cin >> num;
+
+ 
+
+                 if (num == 1)
+
+                         one++;
+
+                 else if (num > 0)
+
+                         positive.push(num);
+
+                 else if (num == 0)
+
+                         zero++;
+
+                 else
+
+                         negative.push(num);
+
+        }
+
+ 
+
+        //짝이 없는 양수를 위해 1을 곱해준다
+
+        if (positive.size() % 2)
+
+                 positive.push(1);
+
+        //0이 존재한다면 짝이 없는 음수에 0을 곱해준다
+
+        //0이 존재하지 않는다면 짝이 없는 음수에 1을 곱해준다
+
+        if (negative.size() % 2)
+
+                 zero == 0 ? negative.push(1) : negative.push(0);
+
+        int sum = 0;
+
+        //양수들을 합해주고
+
+        while(!positive.empty())
+
+        {
+
+                 int num1 = positive.top();
+
+                 positive.pop();
+
+                 int num2 = positive.top();
+
+                 positive.pop();
+
+ 
+
+                 sum += (num1*num2);
+
+        }
+
+        //음수들을 합해준다
+
+        while(!negative.empty())
+
+        {
+
+                 int num1 = negative.top();
+
+                 negative.pop();
+
+                 int num2 = negative.top();
+
+                 negative.pop();
+
+                
+
+                 sum += (num1*num2);
+
+        }
+
+        //1들은 그냥 더해준다
+
+        cout << sum + one << "\n";
+
+        return 0;
+
+}    
+
+
+
+//출처: https://jaimemin.tistory.com/723 [꾸준함]
