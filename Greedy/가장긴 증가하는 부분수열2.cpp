6013b791c63d8c6ae02f5b5fec4c95b5591baf88
EunSeong <seoun4612@gmail.com>
2021-04-09 22:53:41 +0900

    practice_21.04.09

---
+++ Greedy/가장긴 증가하는 부분수열2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+vector <int> v;
+
+int main(){
+	cin>>n;
+	v.push_back(0);
+	
+	for(int i=0;i<n;i++){
+		int x;
+		cin>>x;
+		if(v.back()<x)v.push_back(x);
+		else{
+			int index =lower_bound(v.begin(),v.end(),x)-v.begin();
+			v[index] = x;
+		}
+	}
+	cout<<v.size()-1<<endl;
+}
